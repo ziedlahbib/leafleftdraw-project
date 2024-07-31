8b650fb5a138fe5c43645cb8173d5a6e6431b557
ziedlahbib <72930782+ziedlahbib@users.noreply.github.com>
2024-07-31 21:28:14 +0100

    up

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -136,3 +136,25 @@ export class MapComponent implements AfterViewInit {
     });
   }
 }
+// googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+//   maxZoom: 20,
+//   subdomains:['mt0','mt1','mt2','mt3']
+// });
+// Hybrid:
+
+// googleHybrid = L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
+//   maxZoom: 20,
+//   subdomains:['mt0','mt1','mt2','mt3']
+// });
+// Satellite:
+
+// googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+//   maxZoom: 20,
+//   subdomains:['mt0','mt1','mt2','mt3']
+// });
+// Terrain
+
+// googleTerrain = L.tileLayer('http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',{
+//   maxZoom: 20,
+//   subdomains:['mt0','mt1','mt2','mt3']
+// });
